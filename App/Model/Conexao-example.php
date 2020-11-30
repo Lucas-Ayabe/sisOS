db6f90063183a5a8f3a094edad9576fc514ba21a
GuilhermeDischer <discher.guilherme@hotmail.com>
2020-11-30 16:51:46 -0300

    Conexao com a Base de Dados

---
+++ App/Model/Conexao-example.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Model;
+
+use PDO;
+
+class Conexao {
+    private static $instance;
+
+    public static function getConn() {
+        if(!isset(self::$instance)) {
+            self::$instance = new PDO("mysql:host=localhost;dbname=seu_banco;charset=utf-8","seu_usuario","sua_senha");
+        }
+        return self::$instance;
+    }
+}
+?>
